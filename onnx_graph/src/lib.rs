f87a71fe35bef2f38d5f6794f90332ff53123539
MihneaDarie <dmihnead15@gmail.com>
2026-03-08 18:51:28 +0200

    added onnx_graph as dependency

--- onnx_graph/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod nodes;
-pub mod tensor_map;
-pub mod typed_array;
-pub mod graph;
--- onnx_graph/src/nodes/hash_trait.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use std::collections::HashMap;
-
-use onnx_extractor::AttributeValue;
-
-pub trait FromHashMap: Sized {
-    fn from_hashmap(attrs: &HashMap<String, AttributeValue>) -> Result<Self>;
-}
